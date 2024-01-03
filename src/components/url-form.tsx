6eb593391c604c787e53ffbde4e1858253120749
Alex Kates <alex.kates@croissant.com>
2024-01-03 05:27:23 -0500

    Add Toaster component and useToast hook

--- src/components/url-form.tsx
+++ src/components/url-form.tsx
@@ -15,13 +15,15 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { toast } from "@/components/ui/use-toast";
+import { useToast } from "@/components/ui/use-toast";
 
 const FormSchema = z.object({
   url: z.string().url("Please enter a valid URL e.g. https://alexkates.dev"),
 });
 
 export default function UrlForm() {
+  const { toast } = useToast();
+
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
     defaultValues: {
@@ -29,14 +31,30 @@ export default function UrlForm() {
     },
   });
 
-  function onSubmit(data: z.infer<typeof FormSchema>) {
+  async function onSubmit(data: z.infer<typeof FormSchema>) {
+    const res = await fetch("/api/og", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (!res.ok) {
+      const statusText = res.statusText;
+      console.error(statusText);
+      toast({
+        title: "Uh oh! Something went wrong.",
+        description: "There was a problem with your request.",
+      });
+      return;
+    }
+
+    const json = await res.json();
+
     toast({
-      title: "You submitted the following values:",
-      description: (
-        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
-          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
-        </pre>
-      ),
+      title: "Success!",
+      description: "Your OG metadata is ready.",
     });
   }
 
