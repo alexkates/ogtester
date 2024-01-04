f9538f5b63160d98d8074d1d45a307f534e07459
Alex Kates <alex.kates@croissant.com>
2024-01-04 05:41:57 -0500

    Add @radix-ui/react-tabs component

--- src/app/og/page.tsx
+++ src/app/og/page.tsx
@@ -1,4 +1,5 @@
 import { Table, TableBody, TableCell, TableRow } from "@/components/ui/table";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 function Page({
   searchParams,
@@ -6,26 +7,49 @@ function Page({
   searchParams: { [key: string]: string | string[] | undefined };
 }) {
   const ogImage = searchParams["og:image"] as string | undefined;
-  const ogImageWidth = searchParams["og:image:width"] as string;
-  const ogImageHeight = searchParams["og:image:height"] as string;
   const twitterImage = searchParams["twitter:image"] as string | undefined;
-  const twitterImageWidth = searchParams["twitter:image:width"];
-  const twitterImageHeight = searchParams["twitter:image:height"];
 
   return (
     <main>
-      <img src={ogImage} alt="Open Graph Image" />
-      <img src={twitterImage} alt="Twitter Image" />
-      <Table>
-        <TableBody>
-          {Object.entries(searchParams).map(([key, value]) => (
-            <TableRow key={key}>
-              <TableCell>{key}</TableCell>
-              <TableCell>{value}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+      <Tabs defaultValue="Open Graph">
+        <TabsList>
+          <TabsTrigger value="Open Graph">Open Graph</TabsTrigger>
+          <TabsTrigger value="Twitter">Twitter</TabsTrigger>
+        </TabsList>
+        <TabsContent value="Open Graph">
+          <img src={ogImage} alt="Open Graph Image" />
+          <Table>
+            <TableBody>
+              {Object.entries(searchParams)
+                .filter(([key]) => key.startsWith("og:") && key !== "og:image")
+                .map(([key, value]) => (
+                  <TableRow key={key}>
+                    <TableCell>{key}</TableCell>
+                    <TableCell>{value}</TableCell>
+                  </TableRow>
+                ))}
+            </TableBody>
+          </Table>
+        </TabsContent>
+        <TabsContent value="Twitter">
+          <img src={twitterImage} alt="Twitter Image" />
+          <Table>
+            <TableBody>
+              {Object.entries(searchParams)
+                .filter(
+                  ([key]) =>
+                    key.startsWith("twitter:") && key !== "twitter:image"
+                )
+                .map(([key, value]) => (
+                  <TableRow key={key}>
+                    <TableCell>{key}</TableCell>
+                    <TableCell>{value}</TableCell>
+                  </TableRow>
+                ))}
+            </TableBody>
+          </Table>
+        </TabsContent>
+      </Tabs>
     </main>
   );
 }
