1e9e8f1173e1caa1fac2f79e440ce37cea25a401
Alex Kates <alex.kates@croissant.com>
2024-01-04 06:29:47 -0500

    Add Card component to UrlForm

--- src/components/url-form.tsx
+++ src/components/url-form.tsx
@@ -2,6 +2,13 @@ import { notFound, redirect } from "next/navigation";
 import { Input } from "./ui/input";
 import fetchMetaTags from "@/lib/meta-tags";
 import SubmitButton from "./url-form-submit-button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
 
 export default function UrlForm() {
   async function onSubmit(formData: FormData) {
@@ -19,9 +26,19 @@ export default function UrlForm() {
   }
 
   return (
-    <form action={onSubmit}>
-      <Input placeholder="https://alexkates.dev" name="url" type="url" />
-      <SubmitButton />
-    </form>
+    <Card>
+      <CardHeader>
+        <CardTitle>Open Graph Tester</CardTitle>
+        <CardDescription>
+          Test your Open Graph tags for your website.
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form action={onSubmit} className="flex flex-col gap-4">
+          <Input placeholder="https://alexkates.dev" name="url" type="url" />
+          <SubmitButton />
+        </form>
+      </CardContent>
+    </Card>
   );
 }
