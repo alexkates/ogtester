17e63a43e93e166a91a854cb6dbf27ddb9eaa122
Alex Kates <alex.kates@croissant.com>
2024-01-05 11:50:03 -0500

    Refactor header navigation component

--- src/components/header-nav.tsx
+++ src/components/header-nav.tsx
@@ -7,33 +7,20 @@ import {
   NavigationMenuItem,
   NavigationMenuList,
 } from "@/components/ui/navigation-menu";
-import { cn } from "@/lib/utils";
-import { usePathname } from "next/navigation";
+import Logo from "./logo";
 
 export function HeaderNav() {
-  const pathname = usePathname();
-  const isActive = (path: string) => pathname === path.split("?")[0];
-
-  const navs = [
-    {
-      href: "/",
-      label: "home",
-    },
-  ];
-
   return (
     <NavigationMenu>
       <NavigationMenuList className="gap-4">
-        {navs.map((nav) => (
-          <NavigationMenuItem key={nav.href}>
-            <Link
-              href={nav.href}
-              className={cn(isActive(nav.href) && "underline")}
-            >
-              {nav.label}
-            </Link>
-          </NavigationMenuItem>
-        ))}
+        <NavigationMenuItem>
+          <Link href="/" className="flex items-center">
+            <Logo width={30} height={30} />
+            <span className="ml-1 text-lg font-semibold">
+              og<span className="text-primary">tester</span>
+            </span>
+          </Link>
+        </NavigationMenuItem>
       </NavigationMenuList>
     </NavigationMenu>
   );
