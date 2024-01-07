8e3d9285b1e783fc9e7781f44645298d02ebe7f9
Alex Kates <alex.kates@croissant.com>
2024-01-07 14:23:58 -0500

    Refactor OG page and OgCardList component

--- src/components/og-card-list.tsx
+++ src/components/og-card-list.tsx
@@ -8,18 +8,25 @@ type Props = {
 
 function OgCardList({ metaTagDefinitions }: Props) {
   return (
-    <div className="flex gap-4">
-      {[
-        metaTagDefinitions.slice(0, metaTagDefinitions.length / 2),
-        metaTagDefinitions.slice(metaTagDefinitions.length / 2, metaTagDefinitions.length),
-      ].map((metaTagDefinitionGroup, index) => (
-        <div key={index} className="flex flex-col gap-4">
-          {metaTagDefinitionGroup.map((metaTagDefinition) => (
-            <OgCard key={metaTagDefinition.name} metaTagDefinition={metaTagDefinition} />
-          ))}
-        </div>
-      ))}
-    </div>
+    <>
+      <div className="flex flex-col gap-4 md:hidden">
+        {metaTagDefinitions.map((metaTagDefinition) => (
+          <OgCard key={metaTagDefinition.name} metaTagDefinition={metaTagDefinition} />
+        ))}
+      </div>
+      <div className="hidden gap-4 md:flex">
+        {[
+          metaTagDefinitions.slice(0, metaTagDefinitions.length / 2),
+          metaTagDefinitions.slice(metaTagDefinitions.length / 2, metaTagDefinitions.length),
+        ].map((metaTagDefinitionGroup, index) => (
+          <div key={index} className="flex w-[380px] flex-col gap-4">
+            {metaTagDefinitionGroup.map((metaTagDefinition) => (
+              <OgCard key={metaTagDefinition.name} metaTagDefinition={metaTagDefinition} />
+            ))}
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
 
